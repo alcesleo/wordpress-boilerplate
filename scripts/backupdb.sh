05008f48c215fc824a1764948242684feaa639aa
alcesleo <lagginglion@gmail.com>
2013-08-12 16:08:51 +0200

    A script to backup the database and stage for commit

---
+++ scripts/backupdb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+# This script makes a textfile containing the database structure and data,
+# and stages it in git. This way the database can be kept under version control
+# alongside the rest of your code.
+#
+# It can be used either manually, or as a pre-commit hook (see README.md)
+
+# Configuration
+# Change these to what works for you
+user=root
+password=root
+database=database
+
+# Create the database backup
+mysqldump --skip-extended-insert --user=$user --password=$password $database > www/database/database.sql
+git add www/database/database.sql
