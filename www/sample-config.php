6d5413c02f39fc9dbe03706bf6f39384c70e1241
alcesleo <lagginglion@gmail.com>
2013-10-09 20:54:50 +0200

    Added sample config

---
+++ www/sample-config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file contains what should be in the local-config.php,
+ * dev-config.php, staging-config.php or production-config.php
+ * when you're using alcesleo/wordpress-boilerplate.
+ *
+ * Edit these settings to use your credentials, and rename this file
+ * to the appropriate filename.
+ */
+
+/** MySql database name */
+define('DB_NAME', 'database');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
